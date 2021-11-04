d88c8d07df298f617851a655e620a3cfc7d6ce1e
Shisato <shisatoyano@gmail.com>
2021-11-04 22:46:23 +0900

    implemented find value sample

--- src/full_search/linear_search_method/find_value.py
+++ src/full_search/linear_search_method/find_value.py
@@ -1,10 +1,24 @@
+import random
 
 def main():
-    N = input("size of sequence: ")
+    N = int(input("size of sequence: "))
     
-    v = input("target value: ")
+    v = int(input("target value: "))
     
     print("N:{} v:{}".format(N, v))
+    
+    a = random.sample(range(100), k=N)
+    print(a)
+    
+    # linear search
+    exist = False
+    for i in range(N):
+        if a[i] == v:
+            exist = True
+    
+    # output result
+    if exist: print("Yes")
+    else: print("No")
 
 if __name__ == "__main__":
     main()
