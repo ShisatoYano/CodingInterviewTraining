450b9cd3ff8988ac854a822563248416e7bcb986
Shisato <shisatoyano@gmail.com>
2021-11-06 20:57:44 +0900

    add sum until n calculation recursively

---
+++ src/recursive/sum_n.py
@@ -0,0 +1,17 @@
+def func(N):
+    # report recursive function was called
+    print("func({}) was called".format(N))
+    
+    if N == 0: return 0
+    
+    # calculate recursively
+    result = N + func(N - 1)
+    print("Sum until {} = {}".format(N, result))
+    
+    return result
+
+def main():
+    func(5)
+
+if __name__ == "__main__":
+    main()
