e6811cac7b60b80832d2cf23bc5c9fe774053d9e
Shisato <shisatoyano@gmail.com>
2021-11-19 13:18:27 +0900

    implemented insert function

--- src/data_structure/linked_list/linked_list.py
+++ src/data_structure/linked_list/linked_list.py
@@ -14,16 +14,42 @@ class Node:
 class LinkedList:
     # 初期化
     def __init__(self):
-        self.head = Node(None)
+        self.nil = Node(None) # 番兵
+        self.nil.next = self.nil
     
-    # 挿入操作
-    # ポインタの矢印を繋ぎ変えることで実現する
-    def insert(self, name):
-        front = self.head
-        rear = front.next
+    # 連結リストを出力する
+    def print_list(self):
+        cur = self.nil.next # 先頭から出発
+        while cur != self.nil:
+            print("{} -> ".format(cur.name), end=' ')
+            cur = cur.next
+        print('')
+    
+    # ノードpの直後にノードvを挿入する
+    # ノードpのデフォルト引数をnilとしておく
+    # そのためinsert(v, nil)を呼び出す操作はリストの先頭への挿入を表す
+    def insert(self, v: Node, p: Node):
+        v.next = p.next
+        p.next = v
 
 def main():
+    # 初期化
     l = LinkedList()
+    
+    # 作りたいノードの名前一覧
+    # 最後尾のノードから順に挿入する
+    names = ["yamamoto", "watanabe", "ito", "takahashi", "suzuki", "sato"]
+    
+    for i, name in enumerate(names):
+        # ノードを作成する
+        node = Node(name)
+        
+        # 作成したノードを連結リストの先頭に挿入する
+        l.insert(node, l.nil)
+        
+        # 各ステップの連結リストの様子を出力する
+        print("step {}:".format(i), end=' ')
+        l.print_list()
 
 if __name__ == "__main__":
     main()
