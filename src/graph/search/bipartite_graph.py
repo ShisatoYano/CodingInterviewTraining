ac287273225e611b8df1e0d4738c744ba22f096a
Shisato <shisatoyano@gmail.com>
2021-12-22 23:00:28 +0900

    implemented bitpartite graph problem

--- src/graph/search/bipartite_graph.py
+++ src/graph/search/bipartite_graph.py
@@ -15,6 +15,34 @@ N = len(graph)
 # 黒か白か
 color = [-1] * N
 
+# 深さ優先探索
+def dfs(graph, v, cur=0):
+    color[v] = cur
+    
+    for next_v in graph[v]:
+        # 隣接頂点がすでに色確定していた場合
+        if color[next_v] != -1:
+            # 同じ色が隣接した場合は二部グラフではない
+            if color[next_v] == cur: return False
+            
+            # 色が確定した場合には探索しない
+            continue
+        
+        # 隣接頂点の色を変えて再帰的に探索
+        # Falseが返ってきたらFalseを返す
+        if dfs(graph, next_v, 1-cur) == False: return False
+    
+    return True
+
 if __name__ == "__main__":
     # 探索
     is_bipartite = True
+    
+    for v in range(N):
+        # vが探索済みの場合は無視
+        if color[v] != -1: continue
+        if dfs(graph, v) == False: is_bipartite = False
+    
+    if is_bipartite: print("Yes")
+    else: print("No")
+        
